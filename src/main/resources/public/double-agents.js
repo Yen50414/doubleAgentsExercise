cb311130a3af0d0b94388d1e88a84afb5b0577a4
Yen <Yen50414@gmail.com>
2016-10-31 07:03:35 -0400

    Differentiate Male vs Female

--- src/main/resources/public/double-agents.js
+++ src/main/resources/public/double-agents.js
@@ -1,6 +1,16 @@
 var mapCenter = [39.09596, -95.80078];
 var defaultZoom = 4;
 
+var maxZ = 18;
+var minZ = 4;
+var mapboxId = 'moaboa';
+var mapboxAT = 'pk.eyJ1IjoibW9hYm9hIiwiYSI6ImNpdXMwcm4wbzAwYXMyeXFqenpxYWJ6bzIifQ.3jc-XUbb1Crk6ruYsOWhxA';
+// Dark map
+var mapUrl = 'https://api.mapbox.com/styles/v1/' + mapboxId + '/ciuxwzl0t004b2ipqrkqoy43p/tiles/256/{z}/{x}/{y}?access_token=' + mapboxAT;
+// Default map from mapbox
+//var mapUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=' + mapboxAT;
+
+
 var mymap;
 var agentsData = [];
 var agentMarkers = [];
@@ -11,27 +21,36 @@ $(document).ready( function () {
 	// Draw map through leafletjs
 	mymap = drawMap();
 	
-	// Somehow obtain data points from csv file
-	drawAgents();
+	// Test point
+	var point = L.circleMarker([40.00579,-83.0278], {
+		color: 'yellow',
+		fillColor: 'yellow',
+		fillOpacity: 0.5,
+		radius: 10
+	}).addTo(mymap);
+	point.bindPopup("<b>Arthur Mo</b><br>26, Male");
+	
+	// Draw initial data points
+	drawData();
 })
 
 function drawMap() {
 	var mymap = L.map('mapid').setView(mapCenter, defaultZoom);
 			
-	L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoibW9hYm9hIiwiYSI6ImNpdXMwcm4wbzAwYXMyeXFqenpxYWJ6bzIifQ.3jc-XUbb1Crk6ruYsOWhxA', {
+	L.tileLayer(mapUrl, {
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> ' +
 			'contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
 			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
-		maxZoom: 18,
-		minZoom: defaultZoom,
-		id: 'moaboa',
-		accessToken: 'pk.eyJ1IjoibW9hYm9hIiwiYSI6ImNpdXMwcm4wbzAwYXMyeXFqenpxYWJ6bzIifQ.3jc-XUbb1Crk6ruYsOWhxA'
+		maxZoom: maxZ,
+		minZoom: minZ,
+		id: mapboxId,
+		accessToken: mapboxAT
 	}).addTo(mymap);
 	
 	return mymap;
 }
 
-function drawAgents() {
+function drawData() {
 	// Get data from application endpoint
 	$.get("/loadAgents", function(data, status) {
 		// For reasons I don't fully understand, agentsData = data doesn't work 
@@ -41,33 +60,37 @@ function drawAgents() {
 			agentsData.push(agent);
 		});
 	}).done( function() {
-		alert("done");
 		drawAgents();
 	});
 }
 
 function drawAgents() {
 	// Loop through all data points
-	alert(agentsData);
-	console.log(agentsData);
-	alert(agentsData.length);
-	console.log(agentsData.length);
 	agentsData.forEach( function(agent) {
-		console.log(agent);
 		agentMarkers.push(drawAgent(agent));
 	});
 }
 
 function drawAgent(agentData) {
 	console.log(agentData);
-	var point = L.marker([agentData.latitude,agentData.longitude]).addTo(mymap);
-	/*var point = L.circle(latlng, {
-		color: 'blue',
-		fillColor: '#36c',
+	
+	//var color = 'Orchid';
+	//var color = 'Aquamarine';
+	var color;
+	if(agentData.gender == "Male") {
+		color = 'Aquamarine';
+	} else {
+		color = 'Orchid';
+	}
+	
+	//var point = L.marker([agentData.latitude,agentData.longitude]).addTo(mymap);
+	var point = L.circleMarker([agentData.latitude,agentData.longitude], {
+		color: color,
+		fillColor: color,
 		fillOpacity: 0.5,
-		radius: 500
-	});
-	point.bindPopup("<b>Jilted Seahorse</b><br>48, Male");*/
+		radius : 10
+	}).addTo(mymap);
+	point.bindPopup("<b>" + agentData.name + "</b><br>" + agentData.age + ", " + agentData.gender);
 	return point;
 }
 
